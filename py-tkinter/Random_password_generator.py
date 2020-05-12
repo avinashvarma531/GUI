dd1637b5e60038a5e1412311296238f9a1a275a7
avinashvarma531 <52931707+avinashvarma531@users.noreply.github.com>
2020-05-12 15:29:32 +0530

    Comments added

--- py-tkinter/Random_password_generator.py
+++ py-tkinter/Random_password_generator.py
@@ -1,15 +1,16 @@
+'''This program is written to generate a random password of length 8 with the help of secrets module.'''
+
 import tkinter as tk
 import secrets 
 import string
 
-def generate():
+def generate(): # generating a random password which startswith('a'-'z'|'A'-'Z')
 	chrs=string.ascii_letters+string.digits+string.punctuation
-	cnt=0
 	x=secrets.SystemRandom().choice(chrs[:52])
 	pwd=''.join([secrets.SystemRandom().choice(chrs) for i in range(7)])
 	label['text']=x+pwd
 
-root=tk.Tk()
+root=tk.Tk()    # root window
 
 canvas=tk.Canvas(root,height=500,width=600)
 canvas.pack()
@@ -17,7 +18,7 @@ canvas.pack()
 frame=tk.Frame(root)
 frame.place(relx=0,rely=0,relwidth=1,relheight=1)
 
-im=tk.PhotoImage(file='landscape.png')
+im=tk.PhotoImage(file='landscape.png')  # for background image
 im_l=tk.Label(frame,image=im)
 im_l.pack()
 
